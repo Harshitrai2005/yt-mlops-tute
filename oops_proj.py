530d2b1e855656968b36d5f392b3d69456c24f12
Harshitrai2005 <harshitrai2532005@gmail.com>
2025-03-11 21:01:32 +0530

    proj baseic structure ready

---
+++ oops_proj.py
@@ -0,0 +1,23 @@
+
+class chatbook:
+    def __init__(self):
+        self.username=" "
+        self.password=" "
+        self.loggedin=False
+        self.menu()
+
+    def menu(self):
+        user_input=input("""wlecome """)
+
+        if(user_input=="1"):
+            pass
+        elif(user_input=="2"):
+            pass
+        elif(user_input=="3"):
+            pass
+        elif(user_input=="4"):
+            pass
+        else:
+            exit()
+
+obj=chatbook()                 
